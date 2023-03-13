5b1266a08d31a8a4df24fd77f506b73bede7b7f6
Dmitry Ilichev <da_ilichev@mail.ru>
2023-03-13 18:43:34 +0300

    Initialize Spring

---
+++ src/main/java/ru/ilichev/webprac/WebpracApplication.java
@@ -0,0 +1,11 @@
+package ru.ilichev.webprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpracApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebpracApplication.class, args);
+	}
+}
